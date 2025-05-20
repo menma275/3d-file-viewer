0886338aaa64d2ad6450c9a961acabd7f31f1973
menma275 <ohsusie9@gmail.com>
2025-05-20 16:58:52 +0900

    add: add color scheme-wip

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -2,12 +2,24 @@ import MainCanvas from './components/Canvas'
 
 function App(): React.JSX.Element {
   return (
-    <div className="mt-3 p-8 w-full h-full overflow-hidden">
-      <div className="flex justify-between items-center">
-        <div>Hi there</div>
-        <div className="text-accent">this is a test</div>
+    <div className="w-full h-full overflow-hidden">
+      <div
+        className="absolute top-0 left-0 p-8 pt-12 w-full h-full flex flex-col
+        justify-between items-center"
+      >
+        <div className="flex flex-row w-full justify-between">
+          <div>Hi there</div>
+          <div className="text-accent">this is a test</div>
+        </div>
+
+        <div className="flex flex-row w-full justify-between">
+          <button className="bg-mg text-bg px-4 py-2 rounded-xl text-xs ">なんらかのボタン1</button>
+          <button className="bg-mg text-accent px-4 py-2 rounded-xl text-xs">
+            なんらかのボタン2
+          </button>
+        </div>
       </div>
-      <div className="w-full h-dvh bg-mg">
+      <div className="absolute top-0 left-0 w-full h-dvh bg-transparent">
         <MainCanvas />
       </div>
     </div>
