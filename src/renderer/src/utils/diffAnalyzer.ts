484f2d2fa40dedd5f13fc6d3af9b1613640ebe3f
pupupu014 <pupupu@pupupu-MacBook-Air.local>
2025-06-04 21:35:21 +0900

    画像ファイル読み込み実装

--- src/renderer/src/utils/diffAnalyzer.ts
+++ src/renderer/src/utils/diffAnalyzer.ts
@@ -5,83 +5,185 @@ import reduceTo3D from './reduceDims'
 //fileDatasgetで保存済みのファイルデータをとってくる
 const dataAnalyze = async (
     analyzedDatas,
-    folderPath,
-    allFiles,
+    graphicFiles,
+    graphicFileContents,
+    textFiles,
     fileContents,
+    gStats,
     stats
 ): Promise<any[]> => {
     const newFileDataList:FileData[] = []
+    let isCustomNeed: boolean = false
+    const unEmbedFiles: string[] = []
     try {
-        for (let i = 0; i < allFiles.length; i++) {
-            const filePath = allFiles[i]
-            const fileContent = fileContents[i]
-            const createdAt = stats[i].birthtime
-            const updatedAt = stats[i].mtime
+        //テキストファイルの処理スタート
+        if(textFiles){
+            for (let i = 0; i < textFiles.length; i++) {
+                const filePath = textFiles[i]
+                const fileContent = fileContents[i]
+                const createdAt = stats[i].birthtime
+                const updatedAt = stats[i].mtime
 
-            // 解析済みデータから該当ファイルを探す
-            const existing = analyzedDatas.find((d) => d.filePath === filePath)
+                // 解析済みデータから該当ファイルを探す
+                const existing = analyzedDatas.find((d) => d.filePath === filePath)
 
-            let needAnalyze = false
+                let needAnalyze = false
 
-            if (!existing) {
-                // 初めてのファイル
-                needAnalyze = true
-            } else {
-                const analyzedAt = existing?.analyzedAt
-                ? new Date(existing.analyzedAt)
-                : new Date('1900-01-01T00:00:00Z')
-
-                if (updatedAt > analyzedAt.toISOString) {
-                    // 更新されたファイル
+                if (!existing) {
+                    // 初めてのファイル
                     needAnalyze = true
-                }
-            }
+                    if(!isCustomNeed){
+                        isCustomNeed = true
+                    }
+                } else {
+                    const analyzedAt = existing?.analyzedAt
+                    ? new Date(existing.analyzedAt)
+                    : new Date('1900-01-01T00:00:00Z')
 
-            if (needAnalyze) {
-                //解析処理は未実装
-
-                //responseとすでにstoreされている、各ファイルのembeddings多次元データを全て取得してきて、PCRにかけて次元削減
-                const response: number[] = await window.api.embedding(fileContent)
-
-                //以下は指定したパラメータに対してテキストモデルに判定してもらう処理
-                //customvectorshema.nameを持ってきてその単語を投げる
-                const customVecSchemas: CustomVectorSchema[] = await window.api.getCustomVectorName()
-                const customVecNames: string[] = customVecSchemas.map(c => c.name)
-                //const customScores: number[] = await window.api.getCustomScore(fileContent, customVecNames)
-
-                // const customVectorValues = customVecSchemas.map((schema, index) => ({
-                //     schemaId: schema.id,
-                //     value: customScores[index] ?? 1
-                // }))
-
-                const fileId = existing?.id || uuidv4()
-
-                const newFileData: FileData = {
-                    id: fileId,
-                    filePath,
-                    fileType: filePath.split('.').pop(),
-                    fileContent,
-                    createdAt: createdAt,
-                    updatedAt: updatedAt,
-                    analyzedAt: new Date(),
-                    vectors: {
-                        embedding: [], // 失敗時の仮データ
-                        embeddingRaw: response,
-                        //customVectorValues
-                        customVectorValues : [{
-                            schemaId: "aaa",
-                            value: 0.2
-                        }]
+                    if (updatedAt > analyzedAt.toISOString) {
+                        // 更新されたファイル
+                        needAnalyze = true
+                        if(!isCustomNeed){
+                            isCustomNeed = true
+                        }
                     }
                 }
-                newFileDataList.push(newFileData)
-            } else {
-                newFileDataList.push(existing)
-            }
 
+                if (needAnalyze) {
+                    const fileName = filePath.split('/').pop() || ''
+                    unEmbedFiles.push(`***${fileName}***${fileContent}`)
+                    //responseとすでにstoreされている、各ファイルのembeddings多次元データを全て取得してきて、PCRにかけて次元削減
+                    const response: number[] = await window.api.embedding(fileContent)
+
+                    const fileId = existing?.id || uuidv4()
+
+                    const newFileData: FileData = {
+                        id: fileId,
+                        filePath,
+                        fileType: filePath.split('.').pop(),
+                        fileContent,
+                        createdAt: createdAt,
+                        updatedAt: updatedAt,
+                        analyzedAt: new Date(),
+                        vectors: {
+                            embedding: [], // 失敗時の仮データ
+                            embeddingRaw: response,
+                            customVectorValues: [
+                                {
+                                    schemaId: "test01",
+                                    value: 0.01
+                                }
+                            ] //仮のデータを入れとく
+                        }
+                    }
+                    newFileDataList.push(newFileData)
+                } else {
+                    newFileDataList.push(existing)
+                }
+            }
         }
 
+
+        //ここから画像ファイルの処理スタート
+        if(graphicFiles){
+            for (let i = 0; i < graphicFiles.length; i++) {
+                const gFilePath = graphicFiles[i]
+                const gFileContent = graphicFileContents[i]
+                const gCreatedAt = gStats[i].birthtime
+                const gUpdatedAt = gStats[i].mtime
+
+                // 解析済みデータから該当ファイルを探す
+                const gExisting = analyzedDatas.find((d) => d.filePath === gFilePath)
+
+                let needAnalyze = false
+
+                if (!gExisting) {
+                    // 初めてのファイル
+                    needAnalyze = true
+                    if(!isCustomNeed){
+                        isCustomNeed = true
+                    }
+                } else {
+                    const analyzedAt = gExisting?.analyzedAt
+                    ? new Date(gExisting.analyzedAt)
+                    : new Date('1900-01-01T00:00:00Z')
+
+                    if (gUpdatedAt > analyzedAt.toISOString) {
+                        // 更新されたファイル
+                        needAnalyze = true
+                        if(!isCustomNeed){
+                            isCustomNeed = true
+                        }
+                    }
+                }
+
+                if (needAnalyze) {
+                    const gFileName = gFilePath.split('/').pop() || ''
+                    unEmbedFiles.push(`***${gFileName}***${gFileContent}`)
+
+                    //画像をvisionモデルのLLMに文字に変換してもらう
+                    const graToText = await window.api.graphicToText(gFilePath)
+                    console.log(graToText)
+                    //responseとすでにstoreされている、各ファイルのembeddings多次元データを全て取得してきて、PCRにかけて次元削減
+                    const gResponse: number[] = await window.api.embedding(graToText)
+                    console.log(gResponse)
+                    const gFileId = gExisting?.id || uuidv4()
+
+                    const newgFileData: FileData = {
+                        id: gFileId,
+                        filePath: gFilePath,
+                        fileType: gFilePath.split('.').pop(),
+                        fileContent: gFileContent,
+                        createdAt: gCreatedAt,
+                        updatedAt: gUpdatedAt,
+                        analyzedAt: new Date(),
+                        vectors: {
+                            embedding: [], // 失敗時の仮データ
+                            embeddingRaw: gResponse,
+                            customVectorValues: [
+                                {
+                                    schemaId: "test01",
+                                    value: 0.01
+                                }
+                            ] //仮のデータを入れとく
+                        }
+                    }
+                    newFileDataList.push(newgFileData)
+                } else {
+                    newFileDataList.push(gExisting)
+                }
+            }
+        }
+
+        // if(isCustomNeed){
+        //     //以下は指定したパラメータに対してテキストモデルに判定してもらう処理
+        //     //customvectorshema.nameを持ってきてその単語を投げる
+        //     const customVecSchemas: CustomVectorSchema[] = await window.api.getCustomVectorName()
+        //     const customVecNames: string[] = customVecSchemas.map(c => c.name)
+        //     const newFileContents = unEmbedFiles.join(', ')
+        //     const scoreContent = await window.api.getCustomScore(newFileContents, customVecNames)
+        //     //出力がうまく返ってきてないからここの修正途中やで
+        //     //確認
+        //     let parsedScores: Record<string, number> = {}
+        //     const scoreJsonMatch = scoreContent.match(/```json\s*([\s\S]*?)\s*```/)
+        //     const scoreJson = scoreJsonMatch ? scoreJsonMatch[1] : null
+        //     if (scoreJson) {
+        //         try {
+        //             parsedScores = JSON.parse(scoreJson)
+        //         } catch (err) {
+        //             console.error('スコアのパースに失敗:', scoreContent)
+        //         }
+        //     }
+
+        //     const customVectorValues = customVecSchemas.map((schema) => ({
+        //         schemaId: schema.id,
+        //         value: typeof parsedScores[schema.name] === 'number' ? parsedScores[schema.name] : 1
+        //     }))
+        // }
+
         console.log('custom解析完了')
+
+        console.log(`newFileDataList:${newFileDataList}`)
 
         //解析後上書き保存できるようにidを持っておく
         const embedVecs = newFileDataList.map(file => ({
@@ -89,10 +191,14 @@ const dataAnalyze = async (
             embeddingRaw: file.vectors.embeddingRaw
         }))
 
+        console.log(`embedVecs:${embedVecs}`)
+
         //pcaへの入力データ
         const rawVecs = embedVecs.map(emb => emb.embeddingRaw)
 
         // responseとembedVecをPCAに入力して戻り値をそれぞれのjsonファイルに上書きする
+        console.log("rawVecs:", rawVecs)
+        console.log("各ベクトルの次元数:", rawVecs.map(v => v.length))
         const reducedVec:number[][] = await reduceTo3D(rawVecs)
 
         const idToReducedVec = new Map<string, number[]>();
